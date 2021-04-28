6ac07749ba470ac3e3ac70b7deefd51afa2c8faf
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-04-28 11:06:46 -0600

    Update 2.0.0
    Make index compatible with both browser and node, cleanup code, improve functionality, debug, and remove setKey. Also removed access to the characters variable when used in browser.

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-var arr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')','-','_','+','=','/','|','{','}','[',']',':',';','"',"'",'.',',','<','>','?',' '];
+(function (factory) {
+	try {
+		module.exports = factory();
+	} catch {
+		window.strToNum = factory();
+	};
+})(function () {
+	let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%()*+,-./\\:;=?@[]^_`{|}~\"'&<> "
+	let exports = {
+		encode: function (s) {
+			let r = "";
+			for (let l of s) {
+				r += (chars.indexOf(l) + 1) < 10 ? `0${chars.indexOf(l)}` : `${chars.indexOf(l)}`;
+			}
+			return `${r}00`;
+		},
+		decode: function(n) {
+			let r = "";
+			let t = n.match(/[^][^]?/g);
+			if (t[t.length - 1] === "00") {
+				t.pop();
+			};
+			for (let p of t) {
+				r += chars[p];
+			}
+			return r;
+		}
+	}
 
-var encode = (str) => {
-  var toReturn = "";
-  for (var i = 0; i < str.length; i++) {
-    if (arr.indexOf(str[i]).toString().length == 1) {
-      toReturn += '0' + (arr.indexOf(str[i]) + 1).toString();
-    } else {
-      toReturn += (arr.indexOf(str[i]) + 1).toString();
-    }
-  }
-  toReturn += '00'
-  return toReturn;
-}
-
-var decode = (num) => {
-  var toReturn = "";
-  for (var i = 0; Number(num[i].toString() + num[i + 1].toString()) > 0; i += 2) {
-	  toReturn += arr[Number(num[i].toString() + num[i + 1].toString())-1]
-  }
-  return toReturn;
-}
-
-var setKey = (key) => {
-	arr = key
-}
-
-module.exports = {
-  setKey: setKey,
-  encode: encode,
-  decode: decode
-}
+	return exports;
+});
