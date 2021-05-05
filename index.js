cd0453ef065ea8cdf5a7cb96cc3edd448beec169
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-05-05 13:10:29 -0600

    Update 2.3.0

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-(function (factory) {
-	try {
-		module.exports = factory();
-	} catch {
-		window.strToNum = factory();
-	};
-})(function () {
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.strToNum = factory()
+  }
+})(this, function () {
 	let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%()*+,-./\\:;=?@[]^_`{|}~\"'&<> "
 	let exports = {
 		encode: function (s) {
@@ -17,13 +19,15 @@
 		decode: function(n) {
 			let r = "";
 			let t = n.match(/[^][^]?/g);
-			if (t[t.length - 1] === "00") {
-				t.pop();
-			};
-			for (let p of t) {
-				r += chars[p];
-			}
-			return r;
+      for (let c in t) {
+        if (t[c] === "00") {
+          r.__proto__.input = t.filter((p, i) => i <= c).join("");
+          return r;
+        }
+        r += chars[t[c]];
+      }
+      r.__proto__.input = n;
+      return r;
 		}
 	}
 
