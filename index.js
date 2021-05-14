5b4a2294f2569949960d713c988dc0d91c690d2f
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-05-14 15:08:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory)
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory()
-  } else {
-    root.strToNum = factory()
-  }
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = factory();
+    } else {
+        root.strToNum = factory();
+    }
 })(this, function () {
-	let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%()*+,-./\\:;=?@[]^_`{|}~\"'&<> "
-	let exports = {
-		encode: function (s) {
-			let r = "";
-			for (let l of s) {
-				r += (chars.indexOf(l) + 1) < 10 ? `0${chars.indexOf(l) + 1}` : `${chars.indexOf(l) + 1}`;
-			}
-			return `${r}00`;
-		},
-		decode: function(n, l = 0, o = false) {
-			let r = "";
-      l = o && l > 0 ? l - 1 : l;
-			let t = n.slice(l).match(/[^][^]?/g);
-      for (let c in t) {
-        if (t[c] === "00") {
-          return r;
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%()*+,-./\\:;=?@[]^_`{|}~\"'&<> ";
+    let exports = {};
+    exports.encode = function (s) {
+        let r = "";
+        for (let l of s) {
+            r += chars.indexOf(l) + 1 < 10 ? `0${chars.indexOf(l) + 1}` : `${chars.indexOf(l) + 1}`;
         }
-        r += chars[t[c] - 1];
-      }
-      return r;
-		}
-	}
+        return `${r}00`;
+    };
+    exports.decode = function (n, l = 0, o = false) {
+        let r = "";
+        l = o && l > 0 ? l - 1 : l;
+        let t = n.slice(l).match(/[^][^]?/g);
+        for (let c in t) {
+            if (t[c] === "00") {
+                return r;
+            }
+            r += chars[t[c] - 1];
+        }
+        return r;
+    };
 
-	return exports;
+    return exports;
 });
